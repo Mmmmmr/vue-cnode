1eb9455ff253570c8ec3d6e5341fbd57fef0be4d
home <jianglimin999@gmail.com>
2019-06-23 09:08:15 +0800

    页面组件完成

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,23 +1,25 @@
 <template>
-  <nav :style="styleObj">
+  <nav>
     <ul>
-      <li>
-        <a href>全部</a>
+      <li :class="{active:$route.params.tab===undefined}">
+        <router-link to="/">全部</router-link>
+      </li>
+      <li :class="{active:$route.params.tab==='good'}">
+        <router-link :to="{name:'category',params:{tab:'good'}}">精华</router-link>
+      </li>
+      <li :class="{active:$route.params.tab==='share'}">
+        <router-link :to="{name:'category',params:{tab:'share'}}">分享</router-link>
       </li>
       <li>
-        <a href>精华</a>
+        <router-link to="/">
+          <img src="@/assets/cnodejs_light.svg" alt="cnode logo">
+        </router-link>
       </li>
-      <li>
-        <a href>分享</a>
+      <li :class="{active:this.$route.params.tab==='ask'}">
+        <router-link :to="{name:'category',params:{tab:'ask'}}">问答</router-link>
       </li>
-      <li>
-        <a href=""><img src="@/assets/cnodejs_light.svg" alt="cnode logo"></a>
-      </li>
-      <li>
-        <a href>问答</a>
-      </li>
-      <li>
-        <a href>招聘</a>
+      <li :class="{active:$route.params.tab==='job'}">
+        <router-link :to="{name:'category',params:{tab:'job'}}">招聘</router-link>
       </li>
     </ul>
     <svg xmlns="http://www.w3.org/2000/svg">
@@ -29,7 +31,12 @@
           <feDropShadow dx="2" dy="2" stdDeviation="3"></feDropShadow>
         </filter>
       </defs>
-      <rect width="100%" height="2rem" fill="url(#irregularLine)" style="filter:url(#irregularLine-shadow)"></rect>
+      <rect
+        width="100%"
+        height="2rem"
+        fill="url(#irregularLine)"
+        style="filter:url(#irregularLine-shadow)"
+      ></rect>
     </svg>
   </nav>
 </template>
@@ -37,58 +44,90 @@
 <script>
 export default {
   name: "navbar",
-  data:function(){
-    return {
-      styleObj:{
-        padding:"0px",
-        margin:"0px",
-        boxSizing:"border-box",
-        fontSize:"14px",
-        fontFamily:"'微软雅黑',tahoma, arial, sans-serif"
-      }
-    }
+  data: function() {
+    return {};
   }
 };
 </script>
 
 <style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-size: 10px;
+}
+
 ul,
 li {
   list-style: none;
 }
-a {
+li a {
   text-decoration: none;
   color: inherit;
 }
-a:hover{
-  color:#fff;
+
+li a:hover {
+  color: #fff;
 }
+
+.active {
+  color: #fff;
+  position: relative;
+}
+
+.active::before {
+  content: "";
+  width: 2.5rem;
+  border: 1px solid #fff;
+  display: inline-block;
+  position: absolute;
+  bottom: -5px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
 nav {
   background: #445b55;
   position: relative;
+  width: 100%;
 }
+
 ul {
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-end;
   color: #e6ddd8;
-  margin:0;
+  margin: 0;
 }
-li{
-  margin:0.8rem 1.2rem;
+
+li {
+  margin: 0.8rem 0.3rem;
   vertical-align: text-bottom;
 }
+
 img {
   width: 120px;
   height: 28px;
 }
+
 svg {
   width: 100%;
   height: 2rem;
   position: absolute;
 }
+
 pattern {
   width: 100%;
   height: 100%;
+}
+
+@media screen and (min-width: 500px) {
+  * {
+    font-size: 14px;
+  }
+  li {
+    margin: 0.8rem 1.2rem;
+  }
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import PostList from "@/views/postList.vue";
+import ArticlePost from "@/views/article.vue";
+import Userinfo from "@/views/userInfo.vue";
 
 Vue.use(Router);
 
@@ -9,7 +11,22 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: PostList
+    },
+    {
+      path: "/tab=:tab",
+      name: "category",
+      component: PostList
+    },
+    {
+      path: "/article/:articleId",
+      name: "article-post",
+      component: ArticlePost
+    },
+    {
+      path: "/user/:userId",
+      name: "userinfo",
+      component: Userinfo
     }
   ]
 });
