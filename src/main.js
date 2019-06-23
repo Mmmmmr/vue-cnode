6ebd27fe3ffad5d238d0a0cf7cd0869b25cc121c
home <jianglimin999@gmail.com>
2019-06-23 09:32:50 +0800

    final version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import axios from "axios";
 import router from "./router";
+import axios from "axios";
 import $ from "jquery";
 
 Vue.config.productionTip = false;
